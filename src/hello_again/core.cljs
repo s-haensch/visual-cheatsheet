ca535fceb8e5502a3c2af7335a222b46b9ecf8df
Steffen@Homer <mail@steffen.haensch.de>
2017-10-03 20:53:38 +0200

    dot and rect shapes

--- src/hello_again/core.cljs
+++ src/hello_again/core.cljs
@@ -6,68 +6,99 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom
-  {
-    :text "Hello there!"
-    :name "Steffen"
-  }))
+  { :text "Hello!" })
+)
 
 
-
+;; COMPONENTS
 ;; dot
-(defn dot
-  [color]
-  [:span {:class (str "bubble " color)}])
+(defn dot [color]
+  [:span {:class (str "item dot " color)}]
+)
+
+;; rect
+(defn rect [color]
+  [:span {:class (str "item rect " color)}]
+)
 
 
 ;; array of dots
 (defn item-array
-  ([colors]
-  (for [color colors]
-    (dot color)))
-  
-  ([color times]
+  ([items]
+    (for [item items]
+      ((get item :shape) (get item :color))))
+  ([item times]
     (for [num (range times)]
-      (dot color)))
-  )
+      ((get item :shape) (get item :color)))))
 
 
 ;; card container
-(defn card
-  [props]
-
+(defn card [props]
   [:div.card
     [:div.function
       [:p 
-        "(" [:span.function-name "map"]]
-
+        "(" [:span.function-name (get props :title)]]
       (for [line (get props :lines)]
-        [:p.inset-1 (cons "" line)])
-    ]
+        [:p.inset-1 (cons "" line)])]
 
     [:div.output
-      [:p (cons "=> " (get props :output))]
-    ]
-  ]
+      [:p (cons "=> " (get props :output))]]]
 )
 
-
-(def properties {
+;; props
+(def properties-map {
   :title "map"
-  
   :lines [
     "make-blue"
-
-    [
-      "["
-      (item-array ["green" "purple" "blue" "yellow" "red"])
+    [ "["
+      (item-array [
+        {:color "green"  :shape dot}
+        {:color "purple" :shape rect}
+        {:color "blue"   :shape dot}
+        {:color "yellow" :shape dot}
+        {:color "red"    :shape rect}])
       "])"
-    ]
-  ]
-  
+    ]]
   :output [
-    "(" (item-array "blue" 5) ")"
-  ]}
+    "("
+    (item-array [
+        {:color "blue"  :shape dot}
+        {:color "blue" :shape rect}
+        {:color "blue"   :shape dot}
+        {:color "blue" :shape dot}
+        {:color "blue"    :shape rect}])
+    ")" ]}
+)
+(def properties-filter {
+  :title "filter"
+  :lines [
+    "red?"
+    [ "["
+      (item-array [
+        {:color "green" :shape dot}
+        {:color "red" :shape rect}
+        {:color "blue" :shape rect}
+        {:color "yellow" :shape dot}
+        {:color "red" :shape dot}])
+      "])" ] ]
+  :output [
+    "("
+    (item-array [
+        {:color "red" :shape rect}
+        {:color "red" :shape dot}])
+    ")" ]}
 )
 
-(reagent/render-component [card properties]
+
+;; main
+(defn app []
+  [:div
+    (card properties-map)
+    (card properties-filter)
+  ]
+)
+
+
+;; render
+(reagent/render-component [ app ]
   (. js/document (getElementById "app")))
