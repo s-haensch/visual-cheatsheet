3a5c4b246cf2c624b528c8680049d6a5a275e805
Steffen@Homer <mail@steffen.haensch.de>
2017-10-11 21:01:36 +0200

    card content by properties

--- src/hello_again/core.cljs
+++ src/hello_again/core.cljs
@@ -7,7 +7,6 @@
 (defonce app-state (atom
   { :text "Hello!" }))
 
-
 ;; COMPONENTS
 ;; dot
 (defn dot [color]
@@ -31,10 +30,16 @@
   [:div.card
     [:div.card-header
       [:table
-        [:thead [:tr [:th "Hello"]]]
+        [:thead [:tr [:th (get props :title)]]]
         [:tbody [:tr
-          [:td.package "clojure.core"]
-          [:td.since "since 1.0(" [:a {:href "www.example.com"} "source"] ")"]]]]]
+          [:td.package (get props :package)]
+          [:td.since
+            "since 1.0 ( "
+            [:a {
+                :href (get props :source-link)
+                :target "blank"
+              } "source"]
+            " )"]]]]]
 
     [:div.card-body
       [:div.function
@@ -43,14 +48,20 @@
         (for [line (get props :lines)]
           [:p.inset-1 (cons "" line)])]
       [:div.output
-        [:p (cons "=> " (get props :output))]]]
+        [:p (cons "=>  " (get props :output))]]]
 
     [:div.card-footer
-      [:a {:href "www.example.com"} "Examples"]]])
+      [:a {
+          :href (get props :examples-link)
+          :target "blank"
+        } "» Examples"]]])
 
 ;; props "map"
 (def properties-map {
   :title "map"
+  :package "clojure.core"
+  :since "1.0"
+  :source-link "https://github.com/clojure/clojure/blob/clojure-1.9.0-alpha14/src/clj/clojure/core.clj#L2700"
   :lines [
     "make-blue"
     [ "["
@@ -70,12 +81,16 @@
         {:color "blue"  :shape dot}
         {:color "blue"  :shape dot}
         {:color "blue"  :shape rect}])
-    ")"
-  ]})
+    ")"]
+  :examples-link "http://clojuredocs.org/clojure.core/map"
+  })
 
 ;; props "filter"
 (def properties-filter {
   :title "filter"
+  :package "clojure.core"
+  :since "1.0"
+  :source-link "https://github.com/clojure/clojure/blob/clojure-1.9.0-alpha14/src/clj/clojure/core.clj#L2766"
   :lines [
     "purple?"
     [ "["
@@ -91,11 +106,13 @@
     (item-array [
         {:color "purple"  :shape dot}
         {:color "purple"  :shape rect}])
-    ")" ]})
+    ")" ]
+  :examples-link "http://clojuredocs.org/clojure.core/filter"
+  })
 
 ;; main
 (defn app []
-  [:div
+  [:div.content
     [:h1.headline "The Visual Cheatsheet — " [:span.emph "ClojureScript"]]
     (card properties-map)
     (card properties-filter)
