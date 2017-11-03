5c962f95d8c1395573f23f3c2c3a10db31e469c7
Steffen@Homer <mail@steffen.haensch.de>
2017-11-03 18:03:57 +0100

    new build.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hello-again "0.1.0-SNAPSHOT"
+(defproject visual-cheatsheet "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -24,16 +24,16 @@
                 ;; The presence of a :figwheel configuration here
                 ;; will cause figwheel to inject the figwheel client
                 ;; into your build
-                :figwheel {:on-jsload "hello-again.core/on-js-reload"
+                :figwheel {:on-jsload "visual-cheatsheet.core/on-js-reload"
                            ;; :open-urls will pop open your application
                            ;; in the default browser once Figwheel has
                            ;; started and compiled your application.
                            ;; Comment this out once it no longer serves you.
                            :open-urls ["http://localhost:3449/index.html"]}
 
-                :compiler {:main hello-again.core
+                :compiler {:main visual-cheatsheet.core
                            :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/hello_again.js"
+                           :output-to "resources/public/js/compiled/visual_cheatsheet.js"
                            :output-dir "resources/public/js/compiled/out"
                            :source-map-timestamp true
                            ;; To console.log CLJS data-structures make sure you enable devtools in Chrome
@@ -44,8 +44,8 @@
                ;; lein cljsbuild once min
                {:id "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/hello_again.js"
-                           :main hello-again.core
+                :compiler {:output-to "resources/public/js/compiled/visual_cheatsheet.js"
+                           :main visual-cheatsheet.core
                            :optimizations :advanced
                            :pretty-print false}}]}
 
